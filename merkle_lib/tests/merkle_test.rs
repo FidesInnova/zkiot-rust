54a6498bd34191b2ea9309ccecfd537b329d7128
alireza mdk <alirezamky@hotmail.com>
2024-06-15 15:36:05 +0430

    add some tests

--- merkle_lib/tests/merkle_test.rs
+++ merkle_lib/tests/merkle_test.rs
@@ -11,6 +11,7 @@ mod test {
         set_uri("mongodb://localhost:27017/");
         set_database_name("DB1");
         let collection = set_collection_name("Col1").unwrap();
+        collection.drop(None).unwrap();
 
         collection.insert_one(UserRecord::new("alireza0".to_string(), 12345, hash_leaf("alireza0".to_string(), 12345)), None).unwrap();
         
@@ -28,15 +29,18 @@ mod test {
     #[should_panic]
     fn merkle_verify_path_failure() {
         reset_all();
+
         set_uri("mongodb://localhost:27017/");
         set_database_name("DB1");
-        set_collection_name("Col1").unwrap();
+        let collection = set_collection_name("Col1").unwrap();
+        collection.drop(None).unwrap();
+
         set_record_number(10);
         insert_random_records().unwrap();
+
         set_query(doc! {"name": "NO_RECORD_NAME"});
         read_query().unwrap();
-        let result = verify_path().unwrap();
-        
+        let result = verify_path().unwrap();        
         assert!(result);
     }
 
@@ -47,7 +51,8 @@ mod test {
         set_uri("mongodb://localhost:27017/");
         set_database_name("DB1");
         let collection = set_collection_name("Col1").unwrap();
-        
+        collection.drop(None).unwrap();
+
         collection.insert_one(UserRecord::new("alireza1".to_string(), 12345, hash_leaf("alireza1".to_string(), 12345)), None).unwrap();
         collection.insert_one(UserRecord::new("alireza1".to_string(), 45668, hash_leaf("alireza1".to_string(), 45668)), None).unwrap();
         collection.insert_one(UserRecord::new("alireza1".to_string(), 55555, hash_leaf("alireza1".to_string(), 55555)), None).unwrap();
