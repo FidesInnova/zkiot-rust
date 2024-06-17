ec768b067df222d142172510982f5eec96e132a0
alireza mdk <alirezamky@hotmail.com>
2024-06-18 00:04:19 +0330

    add new zk_iot lib

---
+++ zk_iot/src/lib.rs
@@ -0,0 +1,23 @@
+// incomplete ZKP scheme with "nalgebra" lib
+// initializes and partially fills matrices A, B, C
+
+use nalgebra::DMatrix;
+
+pub struct ZKPScheme {
+    pub a: DMatrix<u32>,
+    pub b: DMatrix<u32>,
+    pub c: DMatrix<u32>,
+    pub ng: usize,
+    pub no: usize,
+}
+
+impl ZKPScheme {
+    pub fn new(ng: usize, no: usize) -> Self {
+        let size = ng + no + 1;
+        let a = DMatrix::zeros(size, size);
+        let b = DMatrix::zeros(size, size);
+        let c = DMatrix::zeros(size, size);
+
+        ZKPScheme { a, b, c, ng, no }
+    }
+}
