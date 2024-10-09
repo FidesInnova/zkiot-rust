9260fee764b7a7be21f5a66dde53eec35fea6d14
aliz-dvp <aliz@fidesinnova.io>
2024-10-09 10:03:24 +0330

    test

--- zk_iot/run_all.sh
+++ zk_iot/run_all.sh
@@ -1,4 +1,13 @@
-echo "" > t.txt && \
-cargo run -p setup >> t.txt && \
-cargo run -p proof_generation   >> t.txt && \
-cargo run -p proof_verification >> t.txt
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: sh mysh.sh [file_name]"
+  exit 1
+fi
+
+FILENAME="$1"
+
+echo "" > "$FILENAME" && \
+cargo run -p setup >> "$FILENAME" && \
+cargo run -p proof_generation >> "$FILENAME" && \
+cargo run -p proof_verification >> "$FILENAME"
--- zk_iot/src/math.rs
+++ zk_iot/src/math.rs
@@ -18,11 +18,11 @@ use crate::{dsp_poly, field, to_bint, utils::add_random_points};
 // pub const P: u64 = 45151681;
 // pub const GENERATOR: u64 = 61;
 
-pub const P: u64 = 2147195779;
-pub const GENERATOR: u64 = 34;
+// pub const P: u64 = 2147195779;
+// pub const GENERATOR: u64 = 34;
 
-// pub const P: u64 = 181;
-// pub const GENERATOR: u64 = 2;
+pub const P: u64 = 181;
+pub const GENERATOR: u64 = 2;
 
 field!(Mfp, P);
 
